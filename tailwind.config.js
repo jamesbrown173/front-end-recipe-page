db7aa687bd050be7f61d5a688be5f631486e02c3
James Brown <jamesbrown173@gmail.com>
2024-07-11 16:29:18 +0700

    16:29

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontWeight: {
+      thin: "100",
+      hairline: "100",
+      extralight: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+      "extra-bold": "800",
+      black: "900",
+    },
+    fontFamily: {
+      outfit: ["outfit", "sans-serif"],
+      youngserif: ["young-serif", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
